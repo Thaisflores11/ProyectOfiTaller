3c763c2fd10482adc621c1d286e0f093d59adba5
Thais11 <thaisflores110620@gmail.com>
2023-11-24 10:58:38 -0400

    cambios thais

---
+++ src/components/Logosito.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Logosito() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Logosito
---
+++ src/components/buttonW.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './stylescomponents/buttonW.css'
+
+function buttonW() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default buttonW
