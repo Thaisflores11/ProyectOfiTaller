e672c1211389d9a48dd4ce8a670fe2ab633d99e4
Thais11 <thaisflores110620@gmail.com>
2023-11-24 12:36:41 -0400

    cambios cimar

--- src/views/VistaHome.jsx
+++ src/views/VistaHome.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Cabezera from '../components/Cabezera'
+import Tarjetarecomendacion from '../components/TarjetaRecomendacion'
 function VistaHome() {
   return (
     <>
         <Cabezera />
+        <Tarjetarecomendacion />
       
     </>
   )
